5f152d0edd295829d0d7a0adacded5abf4d2bea6
Ross-Gosling <rossjgosling@gmail.com>
2016-10-30 20:06:27 +0000

    Added a render window and update loop
    I've laid out a basic update loop and render window to be occupied at a
    later date.

--- Projects/ColonyAI/src/main.cpp
+++ Projects/ColonyAI/src/main.cpp
@@ -7,7 +7,8 @@
 * Final Year Project - Colony-based AI
 */
 
-// Imports
+// Imports 
+// SFML
 #include <SFML/Graphics.hpp>
 #include <SFML/System.hpp>
 #include <SFML/Window.hpp>
@@ -15,7 +16,59 @@
 //!< Entry point for the application
 int main()
 {
+	// Instantiates window
+	sf::RenderWindow window(sf::VideoMode(1280, 720), "ColonyAI - P14141609", sf::Style::Default);
 
+	// Initialises a clock for the update loop
+	sf::Clock clock;
+	// Declares var to track elapsed time
+	sf::Time elapsedTime;
+
+	// While the window is open
+	while (window.isOpen())
+	{
+		// Event object for windows event calls
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			// If Closed event is called
+			if (event.type == sf::Event::Closed)
+			{
+				// Closes window
+				window.close();
+			}
+
+			// If KeyPressed event is called
+			if (event.type == sf::Event::KeyPressed)
+			{
+				// If Esc is pressed
+				if (event.key.code == sf::Keyboard::Escape)
+				{
+					// Closes window
+					window.close();
+				}
+			}
+		}
+
+		// Gets elapsed time from clock
+		elapsedTime = clock.getElapsedTime();
+
+		// Triggers the update loop 128 times a second
+		if (elapsedTime.asMilliseconds() > 1000/128)
+		{
+			// Restarts the clock
+			clock.restart();
+
+			// Update objects
+			// TODO
+		}
+
+		// Draw objects
+		// TODO
+
+		// Displays the current frame
+		window.display();
+	}
 
 	// Finishes 'main' with return
 	return 0;
