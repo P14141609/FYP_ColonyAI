31f15395f0d685829f120bc521843d07801d2946
Ross-Gosling <rossjgosling@gmail.com>
2016-10-30 16:44:34 +0000

    Introduced SFML and altered file structure
    Added SFML-2.4.0 to the solution.
    Changed the project files to reflect the name in VS.

---
+++ Projects/ColonyAI/src/main.cpp
@@ -0,0 +1,22 @@
+/**
+@file main.cpp
+*/
+
+/*! \mainpage
+*
+* Final Year Project - Colony-based AI
+*/
+
+// Imports
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+
+//!< Entry point for the application
+int main()
+{
+
+
+	// Finishes 'main' with return
+	return 0;
+}
