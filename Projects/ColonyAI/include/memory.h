a85c79993290f40c9c82df7d5370eccc2a006eb0
Ross-Gosling <p14141609@my365.dmu.ac.uk>
2017-03-05 03:47:45 +0000

    Added members and updated the constructor to take time, position and type

--- Projects/ColonyAI/include/memory.h
+++ Projects/ColonyAI/include/memory.h
@@ -2,7 +2,8 @@
 #define MEMORY_H
 
 // Imports
-//
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
 
 /////////////////////////////////////////////////
 ///
@@ -13,16 +14,26 @@ class Memory
 {
 private:
 
+	float m_lTime; //!< Memory time
+	sf::Vector2f m_position; //!< Memory position
+	
+	enum MemoryType { OBSTRUCTION, FOOD_SOURCE, WATER_SOURCE }; //!< Enum for Memory types
+	MemoryType m_type; //!< Memory type
+
 protected:
 
 public:
 
 	/////////////////////////////////////////////////
 	///
-	/// \brief Default Constructor
+	/// \brief Constructor
+	///
+	/// \param klTime Time of Memory creation
+	/// \param kPosition Position of the Memory
+	/// \param kType Type of Memory
 	///
 	///////////////////////////////////////////////// 
-	Memory() {}
+	Memory(const long klTime, const sf::Vector2f kPosition, const MemoryType kType);
 };
 
 #endif
--- Projects/ColonyAI/src/memory.cpp
+++ Projects/ColonyAI/src/memory.cpp
@@ -6,7 +6,10 @@
 #include "memory.h"
 
 // Constructor
-Memory::Memory()
+Memory::Memory(const long klTime, const sf::Vector2f kPosition, const MemoryType kType)
 {
-	//
+	// Sets member values to corresponding input
+	m_lTime = klTime;
+	m_position = kPosition;
+	m_type = kType;
 }
